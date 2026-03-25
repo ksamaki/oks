a2100be33825eb983de715f29d0eb4d2bd3c1545
ksamaki <144955732+ksamaki@users.noreply.github.com>
2026-03-25 03:48:39 +0300

    Make endpoint lookup compatible without IEndpointFeature reference

--- src/Oks/Oks.Web/Middleware/OksUnitOfWorkMiddleware.cs
+++ src/Oks/Oks.Web/Middleware/OksUnitOfWorkMiddleware.cs
@@ -1,3 +1,4 @@
+using System.Reflection;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc.Controllers;
 using Oks.Persistence.Abstractions.Repositories;
@@ -22,18 +23,61 @@ public class OksUnitOfWorkMiddleware
         if (context.Response.StatusCode >= StatusCodes.Status500InternalServerError)
             return;
 
-        var endpoint = context.GetEndpoint();
+        var endpoint = ResolveEndpoint(context);
         if (endpoint is null)
             return;
 
         // MVC controller endpoint'lerinde commit işi OksUnitOfWorkFilter'da
-        if (endpoint.Metadata.GetMetadata<ControllerActionDescriptor>() is not null)
+        if (HasMetadata(endpoint, typeof(ControllerActionDescriptor)))
             return;
 
         // Endpoint seviyesinde skip metadata'sı varsa commit etme
-        if (endpoint.Metadata.GetMetadata<OksSkipTransactionAttribute>() is not null)
+        if (HasMetadata(endpoint, typeof(OksSkipTransactionAttribute)))
             return;
 
         await unitOfWork.SaveChangesAsync(context.RequestAborted);
+    }
+
+    private static object? ResolveEndpoint(HttpContext context)
+    {
+        var extensionsType = Type.GetType(
+            "Microsoft.AspNetCore.Http.EndpointHttpContextExtensions, Microsoft.AspNetCore.Http.Abstractions");
+        var getEndpointMethod = extensionsType?.GetMethod(
+            "GetEndpoint",
+            BindingFlags.Public | BindingFlags.Static,
+            binder: null,
+            new[] { typeof(HttpContext) },
+            modifiers: null);
+
+        if (getEndpointMethod is not null)
+            return getEndpointMethod.Invoke(null, new object[] { context });
+
+        foreach (var feature in context.Features)
+        {
+            if (feature.Key.FullName != "Microsoft.AspNetCore.Http.Features.IEndpointFeature")
+                continue;
+
+            return feature.Value?.GetType().GetProperty("Endpoint")?.GetValue(feature.Value);
+        }
+
+        return null;
+    }
+
+    private static bool HasMetadata(object endpoint, Type metadataType)
+    {
+        var metadata = endpoint.GetType().GetProperty("Metadata")?.GetValue(endpoint);
+        if (metadata is null)
+            return false;
+
+        var getMetadata = metadata.GetType().GetMethods()
+            .FirstOrDefault(m =>
+                m.Name == "GetMetadata" &&
+                m.IsGenericMethodDefinition &&
+                m.GetParameters().Length == 0);
+
+        if (getMetadata is null)
+            return false;
+
+        return getMetadata.MakeGenericMethod(metadataType).Invoke(metadata, null) is not null;
     }
 }
